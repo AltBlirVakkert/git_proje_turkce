c2a3c07872716254c82abb5aeaa1fa6b4f20d079
AltBlirVakkert <63270124+AltBlirVakkert@users.noreply.github.com>
2020-06-21 16:52:37 +0300

    Update FirstClass.java

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -4,11 +4,11 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("first commit from local repository");
-		System.out.println("we are again conflict");
+		System.out.println("we are again conflic1t");
 		
 		
 		
-		System.out.println("we want to make second conflict");
+		System.out.println("we want to make second conflic1t");
 		System.out.println("first commit from remote reository");
 	}
 
