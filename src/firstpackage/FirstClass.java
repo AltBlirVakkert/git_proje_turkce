f2f09683bb49ed0ec49235e52ceddb60d3469a1a
AltBlirVakkert <63270124+AltBlirVakkert@users.noreply.github.com>
2020-06-21 16:50:59 +0300

    Update FirstClass.java
    sorun cozme

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -4,8 +4,11 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("first commit from local repository");
-		System.out.println("Second commit from local repository");
-		System.out.println("confilict yapmak istiyoruz1");
+		System.out.println("we are again conflict");
+		
+		
+		
+		System.out.println("we want to make second conflict");
 		System.out.println("first commit from remote reository");
 	}
 
